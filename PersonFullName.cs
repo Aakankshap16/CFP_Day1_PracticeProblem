33d31d6743671d46772510c7d355403e833885d7
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:02:47 +0530

    take firstname, lastname, middle name and find no of parts and and print in 1

---
+++ PersonFullName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class PersonFullName
+    {
+        public static void call()
+        {
+            Console.WriteLine("Enter Your FullName (FirstName MiddleName LastName):");
+            string name = Console.ReadLine();
+            string[] arr= name.Split(" ");
+           
+            Console.WriteLine("No. of parts of Name : " + arr.Length);  
+            foreach (string s in arr) 
+            {
+                
+                Console.Write(s + " ");
+            }
+           
+
+
+        }
+    }
+}
