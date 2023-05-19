7857cc5ebc10ccd94980cf342964bbad09f5cfc6
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:25:59 +0530

    convert no. to string depend on factor it print

---
+++ ConvertNumberToString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class ConvertNumberToString
+    {
+        public static string call(int number)
+        {
+            string result = "";
+
+            if (number % 3 == 0)
+                result += "Pling";
+
+            if (number % 5 == 0)
+                result += "Plang";
+
+            if (number % 7 == 0)
+                result += "Plong";
+
+            if (string.IsNullOrEmpty(result))
+                result = number.ToString();
+
+            return result;
+        }
+    }
+}
