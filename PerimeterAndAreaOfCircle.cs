40c65e761941761e00c8e6356ea9aecbbb59fc04
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:08:25 +0530

    find area and parameter of circle

---
+++ PerimeterAndAreaOfCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class PerimeterAndAreaOfCircle
+    {
+        public static void call()
+        {
+            double parameter, area;
+            Console.WriteLine("Enter the diameter of Circle:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            parameter = (2 * (Math.PI) * d/2);
+            area = (Math.PI) * d / 2 * d / 2;
+            Console.WriteLine("Parameter of the circle is :"+ parameter);
+            Console.WriteLine("Area of the circle is :"+ area);
+
+
+        }
+    }
+}
