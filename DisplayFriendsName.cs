f0c16adc7e0c7fff9585a21387dbb3b3a8b29e52
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 22:43:08 +0530

    Display 5 friends Name

---
+++ DisplayFriendsName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+ public class DisplayFriendsName
+    {
+        public static void Program1()
+        {
+            Console.WriteLine("My 5 Friends Name are :-");
+            Console.WriteLine("Aakanksha");
+            Console.WriteLine("Ritu");
+            Console.WriteLine("Aditya");
+            Console.WriteLine("Anjali");
+            Console.WriteLine("Ashish");
+        }
+    }
+}
