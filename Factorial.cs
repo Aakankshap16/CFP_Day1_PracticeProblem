a8491880534a22b291570da329a08045dd06d47e
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:16:25 +0530

    find factorial of a given no

---
+++ Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+   public class Factorial
+   {
+        public static int fact(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+   }
+}
