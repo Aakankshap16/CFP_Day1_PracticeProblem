ffad30d351e85840a08220f0a52a7089e63c6db5
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 22:52:20 +0530

    print fav place name in caps and small

---
+++ FavouritePlace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class FavouritePlace
+    {
+        public static void call()
+        {
+            Console.WriteLine("Please Enter your's favourite place:");
+            string place = Console.ReadLine();
+         
+            Console.WriteLine("Place name in all upper:"+ place.ToUpper());
+            Console.WriteLine("Place name 1st in upper remains in lower :"+char.ToUpper(place[0])+ place.Substring(1, place.Length - 1).ToLower());
+            Console.WriteLine("Place name in all lower:"+ place.ToLower());
+          
+        }
+    }
+}
