a8424ee74285a9949d3fb14d98fc2a1b1e10bccc
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:14:07 +0530

    find no. div by 7 and multiple of 5

---
+++ DivisibleAndMultipleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+   public class DivisibleAndMultipleTest
+    {
+        public static void call()
+        {
+            for (int i = 2000; i <= 3200; i++)
+            {
+               if (i%7==0 && i%5!=0)
+                {
+                    Console.Write(i+ ",");
+                    
+                }
+            }
+        }
+    }
+}
