a219a67f56b9f4ee02c5bd06e857a6d7cf1b2741
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:18:06 +0530

    print 1-100 without using loop

---
+++ PrintWithOutUsingLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class PrintWithOutUsingLoop
+    {
+        public static void call(int num)
+        {
+            
+            if (num <= 100)
+            {
+                Console.WriteLine(num);
+                call(num + 1);
+            }
+        }
+    }
+}
