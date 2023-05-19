4263816bab22fa26e2ed98a0b04217c01674cc82
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 22:56:11 +0530

    Take no. as string and print 10 time as string

---
+++ TakeNoAsString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class TakeNoAsString
+    {
+        public static void call()
+        {
+            Console.WriteLine("Enter your NUmber:");
+            string number = Console.ReadLine();
+            int newNum = int.Parse(number)*10;
+
+
+            Console.WriteLine($" 10 time of the number is:{newNum.ToString()}");
+        }
+    }
+}
