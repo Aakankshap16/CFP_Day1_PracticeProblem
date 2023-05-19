9a657e9627c18f9d05ade791dd83b31107b8d11b
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:13:06 +0530

    find execution time

---
+++ ExecutionTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class ExecutionTime
+    {
+        public static void call()
+        {
+       
+            var watch = Stopwatch.StartNew();
+            watch.Start();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+            watch.Stop();
+            Console.WriteLine("Execution Time:" + watch.ElapsedMilliseconds);
+        }
+    }
+}
