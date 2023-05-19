b13b174ce5be73b58d18f86287b3c3fa4da20faf
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 22:38:49 +0530

    Main Program For call classes

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Day1_PracticeProblem
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Practice Problem!");
+            //DisplayFriendsName.Program1();
+            //SquareOfSumDiffernce.call();
+            //FavouritePlace.call();
+            //TakeNoAsString.call();
+            //PersonFullName.call();
+            //FindAverage.call();
+            //PerimeterAndAreaOfCircle.call();
+            //ExecutionTime.call();
+            //DivisibleAndMultipleTest.call();
+
+            /*
+            Console.WriteLine("Enter a Number:");
+            int n= Convert.ToInt32(Console.ReadLine());
+            int result = Factorial.fact(n);
+            Console.WriteLine($"Factorial of given no. {n} is {result}");
+            */
+            //PrintWithOutUsingLoop.call(1);
+
+            //RootsOfQuadraticEquation.call();
+            //CheckPresence.call();
+            /*
+            int number = 34;
+            Console.WriteLine(ConvertNumberToString.call(number));
+            number = 30;
+            Console.WriteLine(ConvertNumberToString.call(number));
+            */
+
+            GetNextDay.call();
+            }
+    }
+}
