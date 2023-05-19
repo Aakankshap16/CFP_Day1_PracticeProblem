99e404c3a134327b3cdb08f2acc8c73d8a49c359
Aakankshap16 <aakankshap971@gmail.com>
2023-05-19 23:21:58 +0530

    check a, e, o is present in string or not

---
+++ CheckPresence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_PracticeProblem
+{
+    public class CheckPresence
+    {
+        public static void call()
+        {
+            Console.WriteLine("Enter your string:");
+            string name = Console.ReadLine();
+            bool isAPresent = false;
+            bool isEPresent = false;
+            bool isPPresent = false;
+
+            foreach (char c in name)
+            {
+                if (c == 'a')
+                {
+                    isAPresent = true;
+                }
+                else if (c == 'e')
+                {
+                    isEPresent = true;
+                }
+                else if (c == 'p')
+                {
+                    isPPresent = true;
+                }
+            }
+
+            if (isAPresent && isEPresent && isPPresent)
+            {
+                Console.WriteLine("All Present");
+            }
+            else if (isAPresent || isEPresent || isPPresent)
+            {
+                Console.WriteLine("One or more - Present");
+            }
+            else
+            {
+                Console.WriteLine("None Present");
+            }
+        
+        }
+    }
+}
